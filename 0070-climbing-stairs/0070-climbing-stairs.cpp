b3502edf4b5fe1496f97490111d02aefeeec73c9
ShivamJNU <119946005+ShivamJNU@users.noreply.github.com>
2023-11-30 02:30:05 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (87.31%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
-    
+    // For 2: 2->1->1  or 2->0
     int DP[50];
     
     int func(int n){
         if(n==0){
-            return 1;
+            return 0;
         }
         
         if(n==1){
             return 1;
+        }
+        
+        if(n==2){
+            return 2;
         }
         
         if(DP[n]!=-1){
