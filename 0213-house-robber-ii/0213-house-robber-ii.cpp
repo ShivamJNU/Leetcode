fc7c987e35a296854055c0e31198387a168d3af5
ShivamJNU <119946005+ShivamJNU@users.noreply.github.com>
2023-11-30 01:29:31 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (71.10%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -5,7 +5,16 @@ public:
     
     int DP[105];
     
-    int func(vector<int>&nums, int index){
+    int func(vector<int>&nums, int index, bool flag){
+        
+        if(index==0){
+            if(flag){
+                return 0;
+            }else{
+                return nums[0];
+            }
+        }
+        
         if(index<0){
             return 0;
         }
@@ -14,45 +23,91 @@ public:
             return DP[index];
         }
         
-        
         int ans1,ans2;
         
-            ans1=func(nums, index-1);
-            ans2=func(nums, index-2)+nums[index];
+       
+            ans1=func(nums, index-1, flag);
+            ans2=func(nums, index-2, flag)+nums[index];
+        
+        
         
         return DP[index]=max(ans1, ans2);
     }
     
     int rob(vector<int>& nums) {
+        
         if(nums.size()==1){
             return nums[0];
         }
+            
         memset(DP, -1, sizeof(DP));
-        int last=nums[nums.size()-1];
-        nums.pop_back();
-        
-        int ans1;
-        
-        if(nums.size()==0){
-            ans1=0;
-        }else{
-            ans1=func(nums, nums.size()-1);
-        }
+        int ans1=func(nums, nums.size()-2, false);
         
         memset(DP, -1, sizeof(DP));
-        nums.push_back(last);
-        nums.erase(nums.begin());
+        int ans2=func(nums, nums.size()-3, true)+nums[nums.size()-1];
         
-        int ans2;
-        if(nums.size()==0){
-            ans2=0;
-        }else{
-            ans2=func(nums, nums.size()-1);
-        }
-        
-        int ans=max(ans1, ans2);
+        int ans= max(ans1, ans2);
         
         return ans;
     }
     
 };
+
+// class Solution {
+// public:
+//     // My first intution is to keep a flag whether the last element was selected or not
+//     // Then work accordingly while selecting the first element.
+    
+//     int DP[105];
+    
+//     int func(vector<int>&nums, int index){
+//         if(index<0){
+//             return 0;
+//         }
+        
+//         if(DP[index]!=-1){
+//             return DP[index];
+//         }
+        
+        
+//         int ans1,ans2;
+        
+//             ans1=func(nums, index-1);
+//             ans2=func(nums, index-2)+nums[index];
+        
+//         return DP[index]=max(ans1, ans2);
+//     }
+    
+//     int rob(vector<int>& nums) {
+//         if(nums.size()==1){
+//             return nums[0];
+//         }
+//         memset(DP, -1, sizeof(DP));
+//         int last=nums[nums.size()-1];
+//         nums.pop_back();
+        
+//         int ans1;
+        
+//         if(nums.size()==0){
+//             ans1=0;
+//         }else{
+//             ans1=func(nums, nums.size()-1);
+//         }
+        
+//         memset(DP, -1, sizeof(DP));
+//         nums.push_back(last);
+//         nums.erase(nums.begin());
+        
+//         int ans2;
+//         if(nums.size()==0){
+//             ans2=0;
+//         }else{
+//             ans2=func(nums, nums.size()-1);
+//         }
+        
+//         int ans=max(ans1, ans2);
+        
+//         return ans;
+//     }
+    
+// };
