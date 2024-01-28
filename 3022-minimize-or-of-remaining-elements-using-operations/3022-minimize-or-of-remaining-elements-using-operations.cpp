f83ffcc5c1d4db23379ef3c7261b26d4f26b6ac6
ShivamJNU <119946005+ShivamJNU@users.noreply.github.com>
2024-01-28 22:13:41 +0530

    Time: 157 ms (100.00%), Space: 71.7 MB (60.00%) - LeetHub

---
+++ 3022-minimize-or-of-remaining-elements-using-operations/3022-minimize-or-of-remaining-elements-using-operations.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minOrAfterOperations(vector<int>& nums, int k) {
+        
+        int ans = 0;
+        int mask = 0;
+
+        for(int i=30; i>=0; i--){
+            mask = mask | (1<<i); //mask stores how many bits has been set
+
+            int consecAnd = mask;
+            int cnt = 0;
+
+            for(auto &e: nums){
+                consecAnd = consecAnd & e; 
+
+                if((consecAnd|ans)!=ans){
+                    cnt++;
+                }else{
+                    consecAnd = mask;
+                }
+            }
+
+            if(cnt>k){
+                ans|=(1<<i);
+            }
+        }
+
+        return ans;
+
+    }
+};
