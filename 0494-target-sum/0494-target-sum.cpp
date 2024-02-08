77c09022b02da920c74ce512ed00e22a1ccd6176
ShivamJNU <119946005+ShivamJNU@users.noreply.github.com>
2024-02-08 19:32:30 +0530

    Time: 1000 ms (17.78%), Space: 10.6 MB (68.68%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     
-    int total = 0;
+    // int total = 0;
     
-    void func(int index, int sum, vector<int>&nums, int target){
+    int func(int index, int sum, vector<int>&nums, int target){
         if(index==nums.size()){
             if(sum==target){
-                total++;
+                return 1;
             }
-            return;
+            return 0;
         }
         
-        func(index+1, sum-nums[index], nums, target);
-        func(index+1, sum+nums[index], nums, target);
+        int x1 = func(index+1, sum-nums[index], nums, target);
+        int x2 = func(index+1, sum+nums[index], nums, target);
+        
+        return (x1+x2);
     }
     
     int findTargetSumWays(vector<int>& nums, int target) {
-        func(0, 0, nums, target);
+        int total = 0;
+        total = func(0, 0, nums, target);
         
         return total;
     }
